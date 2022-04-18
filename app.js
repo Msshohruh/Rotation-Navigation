19319e9e4f305ee703235d64af16a00d47a2114b
Msshohruh <msshohruh@gmail.com>
2022-04-18 16:18:58 +0500

    add loading section

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
+const loading = document.querySelector('.loading-container')
 
 open.addEventListener('click', () => {
     container.classList.add('show-nav')
@@ -8,5 +9,23 @@ open.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
     container.classList.remove('show-nav')
-
 })
+
+
+let i = 1
+
+setTimeout(() => {
+    loading.style.opacity = `${i}`
+    appearance(i)
+
+    function appearance(i) {
+        if (i > 0){
+            setTimeout(() => {
+                loading.style.opacity = `${i}`
+                appearance(i - 0.1)
+            }, 200)
+        } else if (i < 0) {
+            loading.style.display = 'none'
+        }
+    } 
+}, 3000)
